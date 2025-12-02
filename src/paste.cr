bba983eb0374919f2bb1e6c3d946e866ab1b6da7
Roberto Alsina <roberto.alsina@gmail.com>
2025-12-02 15:09:41 -0300

    fix: Normalize line endings to LF when creating pastes

--- src/paste.cr
+++ src/paste.cr
@@ -17,6 +17,9 @@ module Pasto
     property user_id : String?
 
     def initialize(@content : String, @language : String? = nil, @theme : String = "default-dark", @ssh_fingerprint : String? = nil, @ssh_ip : String? = nil, @user_id : String? = nil)
+      # Normalize line endings to just '\n'
+      @content = @content.gsub("\r\n", "\n").gsub("\r", "\n")
+
       @created_at = Time.utc
       @updated_at = Time.utc
 
