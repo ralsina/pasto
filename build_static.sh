9e7caad9b1b502b3a9537c6ea1d4f0dc0ca9230d
Roberto Alsina <roberto.alsina@gmail.com>
2025-12-03 12:18:01 -0300

    Add pasto-ssh to static build outputs for both architectures

--- build_static.sh
+++ build_static.sh
@@ -9,8 +9,10 @@ docker run --rm --privileged \
 docker build . -f Dockerfile.static -t pasto-builder
 docker run -ti --rm -v "$PWD":/app --user="$UID" pasto-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --without-development --static -Dinotify --link-flags '-lssh -lssl -lcrypto'"
 mv bin/pasto bin/pasto-static-linux-amd64
+mv bin/pasto-ssh bin/pasto-ssh-static-linux-amd64
 
 # Build for ARM64
 docker build . --platform linux/arm64 -f Dockerfile.static -t pasto-builder
 docker run --platform linux/arm64 -ti --rm -v "$PWD":/app --user="$UID" pasto-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --without-development --static -Dinotify --link-flags '-lssh -lssl -lcrypto'"
 mv bin/pasto bin/pasto-static-linux-arm64
+mv bin/pasto-ssh bin/pasto-ssh-static-linux-arm64
